bd9831d334a64cf80c283d71bcbfd809f4a8b3ad
Andrzej Prochyra <andrzej@prochyra.io>
2026-06-16 19:17:34 +0100

    It works!

--- src/main/java/com/codemanship/refactoring/comments/X1.java
+++ src/main/java/com/codemanship/refactoring/comments/X1.java
@@ -5,7 +5,6 @@ public class X1 {
     static int m(int lowerBound, int upperBound) {
         int p = 0;
 
-        // Iterate from lower bound (q) to upper bound (z)
         for (int i = lowerBound; i <= upperBound; i++) {
             // Add square of each number in the range
             p += n(i);
