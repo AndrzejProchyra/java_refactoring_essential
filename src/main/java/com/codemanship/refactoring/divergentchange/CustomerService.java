f2d1a77d9af9f81e06cf27c8c9e6b8918b57cf6b
Chris Pitts <chris@thirstybear.co.uk>
2026-06-16 20:06:03 +0100

    It works!

--- src/main/java/com/codemanship/refactoring/divergentchange/CustomerService.java
+++ src/main/java/com/codemanship/refactoring/divergentchange/CustomerService.java
@@ -22,13 +22,4 @@ public class CustomerService {
         }
         return "ACTIVE";
     }
-
-    public static class EmailValidator {
-        public static boolean validate(String email) {
-            if (email == null) {
-                return false;
-            }
-            return email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
-        }
-    }
 }
