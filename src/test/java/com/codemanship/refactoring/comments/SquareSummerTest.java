377ac1b3258d7f8918b56e1e6599545e5d06b0cc
Andrzej Prochyra <andrzej@prochyra.io>
2026-06-16 19:28:29 +0100

    It works!

--- src/test/java/com/codemanship/refactoring/comments/SquareSummerTest.java
+++ src/test/java/com/codemanship/refactoring/comments/SquareSummerTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SquareSummerTest {
 
     @Test
-    void t1() {
+    void sumsTheSquaresBetweenValues() {
         int a = 7;
         int b = 12;
 
