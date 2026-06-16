f38373e3a0eb66dbdc5d9d1980f59e3b19898fef
Andrzej Prochyra <andrzej@prochyra.io>
2026-06-16 21:07:43 +0100

    test for unknown type

--- src/test/java/com/codemanship/refactoring/legacycode/ShippingCalculatorTest.java
+++ src/test/java/com/codemanship/refactoring/legacycode/ShippingCalculatorTest.java
@@ -50,4 +50,19 @@ class ShippingCalculatorTest {
 
         assertEquals(27.4, shippingCost);
     }
+
+    @Test
+    void shippingIsMinusOneOnUnknownShippingType() {
+        OrderClient client = (orderId -> new Order(
+                1001,
+                "UNKNOWN_TYPE",
+                2.0,
+                28.0,
+                false));
+        ShippingCalculator calculator = new ShippingCalculator(client);
+
+        double shippingCost = calculator.calculateShipping(1001);
+
+        assertEquals(-1.0, shippingCost);
+    }
 }
